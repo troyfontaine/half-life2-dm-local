4549e7003796935fe81d50bc68b8b7155c965acf
Troy Fontaine <tfontaine@troyfontaine.com>
2021-09-07 23:02:18 -0500

    NO-TICKET - Add Lefthook and fixes
    Signed-off-by: Troy Fontaine <tfontaine@troyfontaine.com>

--- docker-entry.sh
+++ docker-entry.sh
@@ -12,18 +12,16 @@ fi
 
 pushd "${STEAM_APP_DIR}" || return
 
+# shellcheck disable=SC2153
 bash "${STEAM_APP_DIR}/srcds_run" -game "${STEAM_APP_CFG_NAME}" -console -autoupdate \
     -steam_dir "${STEAM_CMD_DIR}" \
     -steamcmd_script "${HOMEDIR}/${STEAM_APP_CFG_NAME}_update.txt" \
     -usercon \
-    +fps_max "${SRCDS_FPSMAX}" \
-    -tickrate "${SRCDS_TICKRATE}" \
     -port "${SRCDS_PORT}" \
     +tv_port "${SRCDS_TV_PORT}" \
     +clientport "${SRCDS_CLIENT_PORT}" \
     +maxplayers "${SRCDS_MAXPLAYERS}" \
     +map "${SRCDS_STARTMAP}" \
-    +sv_setsteamaccount "${SRCDS_TOKEN}" \
     +rcon_password "${SRCDS_RCONPW}" \
     "${PASSWORD_SET}" \
     +sv_region "${SRCDS_REGION}" \
